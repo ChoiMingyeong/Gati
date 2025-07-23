929707761bddf3ea3806df2fe6d6f91f83a7eb6d
ming <alsruddl1119@naver.com>
2025-07-23 14:40:06 +0900

    Project 클래스 생성

--- Projects/DataTableEditor/Common/Models/Project.cs
+++ Projects/DataTableEditor/Common/Models/Project.cs
@@ -1,4 +1,5 @@
-﻿using TSID.Creator.NET;
+﻿using System.Diagnostics.CodeAnalysis;
+using TSID.Creator.NET;
 
 namespace Common.Models
 {
@@ -11,6 +12,21 @@ namespace Common.Models
         public Project()
         {
             TSID = TsidCreator.GetTsid();
+        }
+
+        public List<DataTable> GetDataTables()
+        {
+            return [.. DataTables.Values];
+        }
+
+        public bool TryGetDataTable(Tsid tsid, [NotNullWhen(true)] out DataTable? dataTable)
+        {
+            if (DataTables.TryGetValue(tsid, out dataTable))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
