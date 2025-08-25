413fa5d4e7be4d6882a43945e4b2a802e6d86202
kosukyung <kosukyung94@gmail.com>
2025-08-25 23:25:31 +0900

    파일 재정리

---
+++ Projects/Server/AdminApi/AdminApiInitialize.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ServerLib;
+
+namespace AdminApi
+{
+    public class AdminApiInitialize
+    {
+        [InitializeConfigureServices]
+        private static void InitializeOnStartup(IServiceCollection services)
+        {
+            if (ServerLib.App.Services.Contains("AdminApi") == false)
+            {
+                Logger.Default.LogInformation("fail load admin api");
+            }
+            else
+            {
+                Logger.Default.LogInformation("admin api initialize");
+            }
+        }
+    }
+}
---
+++ Projects/Server/ServerLib/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLib
+{
+    public static class App
+    {
+        private static string _environmentName;
+        public static string ConfigEnvironment => EnvironmentName.ToLower();
+        private static string _services;
+
+        public static bool ContainAdminService => Services.Contains("AdminApi");
+
+        public static string EnvironmentName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_environmentName))
+                {
+                    _environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                }
+
+                return _environmentName;
+            }
+        }
+        public static string Services
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_services))
+                    _services = Environment.GetEnvironmentVariable("SERVICE");
+
+                return _services;
+            }
+        }
+    }
+}
