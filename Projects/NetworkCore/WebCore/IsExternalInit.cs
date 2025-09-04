8b418551271bfcd354a93a3afb97865360cafb86
ming <alsruddl1119@naver.com>
2025-09-05 02:40:54 +0900

    유니티에 호환되게 라이브러리 수정

---
+++ Projects/NetworkCore/WebCore/IsExternalInit.cs
@@ -0,0 +1,8 @@
+﻿// IsExternalInit.cs
+#if !NET5_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    // C# 9 init / record 지원용 폴리필 (참조 전용 타입)
+    internal static class IsExternalInit { }
+}
+#endif
