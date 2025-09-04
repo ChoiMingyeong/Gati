0c8d13dec686b3cc6f513857e9a69a401217062a
ming <alsruddl1119@naver.com>
2025-09-05 00:10:00 +0900

    Revert "서브모듈에 의도하지 않은 파일 삭제"
    This reverts commit 72ea854f048adabfc9af7ddd7eb623998832cf90.
    
    # Conflicts:
    #	Projects/Server/AdminApi/InHouseController.cs
    #	Projects/Server/AdminApi/InHouseRequest.cs
    #	Projects/Server/ProjectLyn/Startup.cs
    #	Projects/Server/ServerLib/CustomAttributeManager.cs
    #	Projects/Server/ServerLib/InitializeConfigureServicesAttribute.cs
    #	Projects/Server/ServerLib/Logger.cs
    #	Projects/Server/ServerLib/ServerInitializer.cs
    #	Projects/Server/ServerLib/ServerLib.csproj

---
+++ Projects/NetworkCore/TestCommon/Shared/S2C/ResponseTest.cs
@@ -0,0 +1,11 @@
+﻿using MemoryPack;
+using WebCore.Packet;
+
+namespace TestCommon.Shared.S2C;
+
+[MemoryPackable]
+[PacketProtocol(Opcode.RESPONSE_TEST)]
+public partial class ResponseTest : IPacket
+{
+    public ResponseCode ResponseCode { get; set; }
+}
---
+++ Projects/NetworkCore/WebCore/Packet/NetworkPacket.cs
@@ -0,0 +1,12 @@
+﻿using MemoryPack;
+
+namespace WebCore.Packet
+{
+    [MemoryPackable]
+    sealed public partial class NetworkPacket
+    {
+        public ushort Opcode { get; set; }
+
+        public byte[] Payload { get; set; } = [];
+    }
+}
