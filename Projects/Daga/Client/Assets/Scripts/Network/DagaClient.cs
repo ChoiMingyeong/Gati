4af1aae7a90e0f88899fe531170e8faa795c196a
ming <alsruddl1119@naver.com>
2025-09-09 23:51:49 +0900

    유니티 R&D

---
+++ Projects/Daga/Client/Assets/Scripts/Network/DagaClient.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using WebCore.Socket.Client;
+
+namespace Assets.Scripts.Network
+{
+    public class DagaClient : MonoBehaviour
+    {
+        private GatiClient<DagaClientRouter> _gatiClient = new();
+        public bool IsConnected => IsConnected;
+
+        private readonly string _address;
+
+        public DagaClient(string ip, int port)
+        {
+            _gatiClient.ConnectAsync($"ws://{ip}:{port}/ws/").GetAwaiter();
+        }
+
+        public void Send(string message)
+        {
+            _gatiClient.SendAsync(new TestCommon.Shared.C2S.RequestTest
+            {
+                Message = message
+            }).GetAwaiter();
+        }
+    }
+}
---
+++ Projects/Daga/Client/Assets/Scripts/Network/DagaClientRouter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using TestCommon.Shared.S2C;
+using WebCore.Packet;
+using WebCore.Socket;
+using WebCore.Socket.Client;
+
+namespace Assets.Scripts.Network
+{
+    public class DagaClientRouter : IClientPacketRouter
+    {
+        public DagaClientRouter(GatiSocket socket) : base(socket)
+        {
+        }
+
+        [PacketHandler]
+        private Task HandleResponseTest(ResponseTest packet)
+        {
+            Console.WriteLine(packet.ResponseCode);
+            Debug.Write(packet.ResponseCode);
+            return Task.CompletedTask;
+        }
+    }
+}
