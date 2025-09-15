82851dad5d9884cb7171132db8bd7d8bb4e6d8df
kosukyung <kosukyung94@gmail.com>
2025-09-16 04:19:51 +0900

    플랫폼 프로젝트 생성 및 docker 세팅

---
+++ Projects/PlatformLyn/PlatformLyn/Server/Controllers/PlatformController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[action]")]
+    public class PlatformController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello() => Ok(new { Message = "OK" });
+    }
+}
